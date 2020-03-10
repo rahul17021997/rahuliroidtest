34282cd9c65c698bf3c9cdb9281214b29e319b31
rahul17021997 <58288283+rahul17021997@users.noreply.github.com>
2020-03-10 23:33:23 +0530

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { JoblistComponent } from './joblist/joblist.component';
+
+
+
+const routes: Routes = [
+  {path:"joblist",component:JoblistComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+import{FormsModule} from '@angular/forms';
+import { JoblistComponent } from './joblist/joblist.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    JoblistComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
